b212a58bf2cb1e07c0c8326b9fe07a1cd05276a6
andrefebrianto <andrefebrianto88@gmail.com>
2015-05-09 15:21:06 +0700

    Membuat Form Input Data SIM

---
+++ QRCodeWinForms/frmInputDataSIM.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace QRCodeWinForms
+{
+    partial class frmInputDataSIM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.txtTempatLahir = new System.Windows.Forms.TextBox();
+            this.txtPendidikan = new System.Windows.Forms.TextBox();
+            this.txtPekerjaan = new System.Windows.Forms.TextBox();
+            this.txtNoSIM = new System.Windows.Forms.TextBox();
+            this.dtpTanggalPembuatan = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpTanggalLahir);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpTanggalPembuatan);
+            this.groupBox1.Controls.Add(this.txtNoSIM);
+            this.groupBox1.Controls.Add(this.txtPekerjaan);
+            this.groupBox1.Controls.Add(this.txtPendidikan);
+            this.groupBox1.Controls.Add(this.txtTempatLahir);
+            this.groupBox1.Controls.Add(this.txtAlamat);
+            this.groupBox1.Controls.Add(this.txtNama);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 236);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data Pribadi Pengemudi";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(123, 19);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(126, 20);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(123, 45);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(214, 20);
+            this.txtAlamat.TabIndex = 2;
+            // 
+            // txtTempatLahir
+            // 
+            this.txtTempatLahir.Location = new System.Drawing.Point(123, 71);
+            this.txtTempatLahir.Name = "txtTempatLahir";
+            this.txtTempatLahir.Size = new System.Drawing.Size(126, 20);
+            this.txtTempatLahir.TabIndex = 3;
+            // 
+            // txtPendidikan
+            // 
+            this.txtPendidikan.Location = new System.Drawing.Point(123, 123);
+            this.txtPendidikan.Name = "txtPendidikan";
+            this.txtPendidikan.Size = new System.Drawing.Size(126, 20);
+            this.txtPendidikan.TabIndex = 5;
+            // 
+            // txtPekerjaan
+            // 
+            this.txtPekerjaan.Location = new System.Drawing.Point(123, 149);
+            this.txtPekerjaan.Name = "txtPekerjaan";
+            this.txtPekerjaan.Size = new System.Drawing.Size(126, 20);
+            this.txtPekerjaan.TabIndex = 6;
+            // 
+            // txtNoSIM
+            // 
+            this.txtNoSIM.Location = new System.Drawing.Point(123, 175);
+            this.txtNoSIM.Name = "txtNoSIM";
+            this.txtNoSIM.Size = new System.Drawing.Size(126, 20);
+            this.txtNoSIM.TabIndex = 7;
+            // 
+            // dtpTanggalPembuatan
+            // 
+            this.dtpTanggalPembuatan.Location = new System.Drawing.Point(123, 201);
+            this.dtpTanggalPembuatan.Name = "dtpTanggalPembuatan";
+            this.dtpTanggalPembuatan.Size = new System.Drawing.Size(150, 20);
+            this.dtpTanggalPembuatan.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Alamat";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tempat Lahir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tanggal Lahir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Pendidikan";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Pekerjaan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nomor SIM";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 207);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Tanggal Pembuatan";
+            // 
+            // dtpTanggalLahir
+            // 
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(123, 97);
+            this.dtpTanggalLahir.Name = "dtpTanggalLahir";
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(150, 20);
+            this.dtpTanggalLahir.TabIndex = 16;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(12, 249);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 1;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(223, 249);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(108, 23);
+            this.btnTampilkan.TabIndex = 2;
+            this.btnTampilkan.Text = "Tampilkan SIM";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            // 
+            // frmInputDataSIM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 284);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmInputDataSIM";
+            this.Text = "InputDataSIM";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpTanggalPembuatan;
+        private System.Windows.Forms.TextBox txtNoSIM;
+        private System.Windows.Forms.TextBox txtPekerjaan;
+        private System.Windows.Forms.TextBox txtPendidikan;
+        private System.Windows.Forms.TextBox txtTempatLahir;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnTampilkan;
+    }
+}
