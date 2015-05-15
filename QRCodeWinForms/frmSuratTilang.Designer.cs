da8e38e7da47738ed1967a69149a211275f81822
imamfzn <imamfzn@gmail.com>
2015-05-15 09:18:10 +0700

    Add EPPlus to project

--- QRCodeWinForms/frmSuratTilang.Designer.cs
+++ QRCodeWinForms/frmSuratTilang.Designer.cs
@@ -1699,7 +1699,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1161, 733);
+            this.ClientSize = new System.Drawing.Size(1161, 736);
             this.Controls.Add(this.btnSimpanData);
             this.Controls.Add(this.btnScanQRCode);
             this.Controls.Add(this.groupBox11);
