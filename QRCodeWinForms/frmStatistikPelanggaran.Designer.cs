9cd6a6f3319d46d39bc8e7729ca8ccacb2ec69a9
imamfzn <imamfzn@gmail.com>
2015-05-22 09:22:12 +0700

    Memisah Statistik Pelanggaran Per Tahun

--- QRCodeWinForms/frmStatistikPelanggaran.Designer.cs
+++ QRCodeWinForms/frmStatistikPelanggaran.Designer.cs
@@ -28,57 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartStatistikPelanggaran = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.btnTampil = new System.Windows.Forms.Button();
+            this.cbbTahun = new System.Windows.Forms.ComboBox();
+            this.lblTahun = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chartStatistikPelanggaran)).BeginInit();
             this.SuspendLayout();
             // 
             // chartStatistikPelanggaran
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartStatistikPelanggaran.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartStatistikPelanggaran.Legends.Add(legend1);
-            this.chartStatistikPelanggaran.Location = new System.Drawing.Point(12, 12);
+            chartArea2.Name = "ChartArea1";
+            this.chartStatistikPelanggaran.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartStatistikPelanggaran.Legends.Add(legend2);
+            this.chartStatistikPelanggaran.Location = new System.Drawing.Point(0, 0);
             this.chartStatistikPelanggaran.Name = "chartStatistikPelanggaran";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Banyak Pelanggaran";
-            this.chartStatistikPelanggaran.Series.Add(series1);
-            this.chartStatistikPelanggaran.Size = new System.Drawing.Size(728, 396);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Banyak Pelanggaran";
+            this.chartStatistikPelanggaran.Series.Add(series2);
+            this.chartStatistikPelanggaran.Size = new System.Drawing.Size(737, 420);
             this.chartStatistikPelanggaran.TabIndex = 0;
             this.chartStatistikPelanggaran.Text = "chart1";
+            this.chartStatistikPelanggaran.Click += new System.EventHandler(this.chartStatistikPelanggaran_Click);
             // 
-            // btnTampil
+            // cbbTahun
             // 
-            this.btnTampil.Location = new System.Drawing.Point(577, 45);
-            this.btnTampil.Name = "btnTampil";
-            this.btnTampil.Size = new System.Drawing.Size(127, 28);
-            this.btnTampil.TabIndex = 1;
-            this.btnTampil.Text = "Lihat Statistik";
-            this.btnTampil.UseVisualStyleBackColor = true;
-            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
+            this.cbbTahun.FormattingEnabled = true;
+            this.cbbTahun.Location = new System.Drawing.Point(616, 60);
+            this.cbbTahun.Name = "cbbTahun";
+            this.cbbTahun.Size = new System.Drawing.Size(83, 21);
+            this.cbbTahun.TabIndex = 2;
+            this.cbbTahun.SelectedIndexChanged += new System.EventHandler(this.cbbTahun_SelectedIndexChanged);
+            // 
+            // lblTahun
+            // 
+            this.lblTahun.AutoSize = true;
+            this.lblTahun.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTahun.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTahun.Location = new System.Drawing.Point(634, 42);
+            this.lblTahun.Name = "lblTahun";
+            this.lblTahun.Size = new System.Drawing.Size(65, 15);
+            this.lblTahun.TabIndex = 3;
+            this.lblTahun.Text = "Pilih Tahun";
             // 
             // frmStatistikPelanggaran
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 420);
-            this.Controls.Add(this.btnTampil);
+            this.ClientSize = new System.Drawing.Size(732, 420);
+            this.Controls.Add(this.lblTahun);
+            this.Controls.Add(this.cbbTahun);
             this.Controls.Add(this.chartStatistikPelanggaran);
             this.Name = "frmStatistikPelanggaran";
             this.Text = "Statistik Pelanggaran";
+            this.Load += new System.EventHandler(this.frmStatistikPelanggaran_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chartStatistikPelanggaran)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartStatistikPelanggaran;
-        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.ComboBox cbbTahun;
+        private System.Windows.Forms.Label lblTahun;
     }
 }
--- QRCodeWinForms/frmStatistikPelanggaran.cs
+++ QRCodeWinForms/frmStatistikPelanggaran.cs
@@ -12,26 +12,11 @@ namespace QRCodeWinForms
 {
     public partial class frmStatistikPelanggaran : Form
     {
+        List<List<int>> dataStatistik = new List<List<int>>();
+        List<DataPelanggaran> data = new List<DataPelanggaran>();
         public frmStatistikPelanggaran()
         {
             InitializeComponent();
-        }
-
-        private void btnTampil_Click(object sender, EventArgs e)
-        {
-            List<DataPelanggaran>data = new  List<DataPelanggaran>();
-            data = ExcelHelper.GetAllPelanggaran();
-            List<int> dataBulan = new List<int>();
-
-            foreach (DataPelanggaran x in data)
-            {
-                dataBulan.Add(x.WaktuPelanggaran.Month);
-            }
-
-            foreach (var grp in dataBulan.GroupBy(i => i))
-            {
-                chartStatistikPelanggaran.Series["Banyak Pelanggaran"].Points.AddXY(ConvertToMonth(grp.Key), grp.Count());
-            }
         }
 
         string ConvertToMonth(int i)
@@ -39,5 +24,53 @@ namespace QRCodeWinForms
             string[] Month = new string[] { "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" };
             return Month[i - 1];
         }
+
+        private void cbbTahun_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chartStatistikPelanggaran.Series["Banyak Pelanggaran"].Points.Clear(); //hapus recent statistik
+
+            /*Membuat Grafik*/
+            foreach (var grp in dataStatistik[cbbTahun.SelectedIndex].GroupBy(i => i))
+            {
+                chartStatistikPelanggaran.Series["Banyak Pelanggaran"].Points.AddXY(ConvertToMonth(grp.Key), grp.Count());
+            }
+        }
+
+        private void frmStatistikPelanggaran_Load(object sender, EventArgs e)
+        {
+            data = ExcelHelper.GetAllPelanggaran();
+            List<int> tahun = new List<int>();
+            List<DateTime> tgl = new List<DateTime>();
+            List<int> bulan = new List<int>();
+
+            /* Memasukkan data tahun dan tanggal ke dalam list tahun dan tanggal dari list data pelanggaran */
+            foreach (DataPelanggaran x in data)
+            {
+                tahun.Add(x.WaktuPelanggaran.Year);
+                tgl.Add(x.WaktuPelanggaran);
+            }
+
+            tahun = tahun.Distinct().ToList(); //membuat elemen list tahun menjadi unik 
+           
+            //Memasukkan Bulan ke dalam list dataStatistik
+            for (int i = 0; i < tahun.Count(); i++)
+            {
+                List<int> sublist = new List<int>();
+                foreach (DateTime value in tgl)
+                {
+                    if (value.Year == tahun[i])
+                    {
+                        sublist.Add(value.Month);
+                    }
+                }
+                dataStatistik.Add(sublist);
+            }
+            
+            cbbTahun.DataSource = tahun; //sumber data combo box (pemilihan tahun statistik)
+        }
+
+        private void chartStatistikPelanggaran_Click(object sender, EventArgs e)
+        {}
+
     }
 }
