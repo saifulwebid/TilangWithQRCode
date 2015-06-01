34629fa2ec2d2a6035e972c76077bbb518863899
imamfzn <imamfzn@gmail.com>
2015-06-01 09:22:23 +0700

    Add Enumeration

---
+++ QRCodeWinForms/Enumeration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCodeWinForms
+{
+    [Flags]
+    public enum EnumJenisUser { Jaksa,Polantas,Samsat }
+    public enum EnumJenisKelamin { Pria,Wanita }
+    public enum EnumPendidikan { SD,SMP,SMA,PTN}
+    public enum EnumPekerjaan { Mahasiswa,Pelajar,PNS,POLRI,Swasta,TNI,Lainnya }
+}
