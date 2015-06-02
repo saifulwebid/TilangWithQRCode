7990d66d53dc06f1910ff0593a2c456bb766cf23
imamfzn <imamfzn@gmail.com>
2015-06-02 11:05:43 +0700

    Banyak Pelanggaran Per SIM

--- QRCodeWinForms/Program.cs
+++ QRCodeWinForms/Program.cs
@@ -17,6 +17,7 @@ namespace QRCodeWinForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+            
         }
     }
 }
