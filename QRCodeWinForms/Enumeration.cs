caa64d0cac641f2891528ace42f9f76acf5d56ee
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-08 10:41:14 +0700

    Memperbaiki Enumeration agar sesuai dengan ClassDiagram

--- QRCodeWinForms/Enumeration.cs
+++ QRCodeWinForms/Enumeration.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 namespace QRCodeWinForms
 {
     [Flags]
-    public enum EnumJenisUser { Jaksa,Polantas,Samsat }
-    public enum EnumJenisKelamin { Pria,Wanita }
-    public enum EnumPendidikan { SD,SMP,SMA,PTN}
-    public enum EnumPekerjaan { Mahasiswa,Pelajar,PNS,POLRI,Swasta,TNI,Lainnya }
+    public enum EnumJenisUser { Jaksa, Polantas, Samsat }
+
+    [Flags]
+    public enum EnumJenisKelamin { Pria, Wanita }
+
+    [Flags]
+    public enum EnumPendidikan { SD, SMP, SMA, PT }
+
+    [Flags]
+    public enum EnumPekerjaan { Mahasiswa, Pelajar, PNS, POLRI, Swasta, TNI, Lainnya }
 }
