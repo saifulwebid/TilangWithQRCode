d613d5a795c6fdb82f145fc6c48d3a3fc5f34cc0
Muhammad Saiful Islam <muhammad@saiful.web.id>
2015-06-21 16:57:00 +0700

    Relayout frmInputDataSIM

--- QRCodeWinForms/frmInputDataSIM.Designer.cs
+++ QRCodeWinForms/frmInputDataSIM.Designer.cs
@@ -81,9 +81,9 @@
             this.groupBox1.Controls.Add(this.txtTempatLahir);
             this.groupBox1.Controls.Add(this.txtAlamat);
             this.groupBox1.Controls.Add(this.txtNama);
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(343, 315);
+            this.groupBox1.Size = new System.Drawing.Size(398, 309);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Data Pribadi Pengemudi";
@@ -94,34 +94,34 @@
             this.cmbPendidikan.FormattingEnabled = true;
             this.cmbPendidikan.Location = new System.Drawing.Point(124, 149);
             this.cmbPendidikan.Name = "cmbPendidikan";
-            this.cmbPendidikan.Size = new System.Drawing.Size(112, 21);
+            this.cmbPendidikan.Size = new System.Drawing.Size(150, 21);
             this.cmbPendidikan.TabIndex = 6;
             // 
             // cmbPekerjaan
             // 
             this.cmbPekerjaan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbPekerjaan.FormattingEnabled = true;
-            this.cmbPekerjaan.Location = new System.Drawing.Point(124, 175);
+            this.cmbPekerjaan.Location = new System.Drawing.Point(124, 176);
             this.cmbPekerjaan.Name = "cmbPekerjaan";
-            this.cmbPekerjaan.Size = new System.Drawing.Size(100, 21);
+            this.cmbPekerjaan.Size = new System.Drawing.Size(150, 21);
             this.cmbPekerjaan.TabIndex = 7;
             // 
             // cmbJenisKelamin
             // 
             this.cmbJenisKelamin.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbJenisKelamin.FormattingEnabled = true;
-            this.cmbJenisKelamin.Location = new System.Drawing.Point(124, 201);
+            this.cmbJenisKelamin.Location = new System.Drawing.Point(124, 203);
             this.cmbJenisKelamin.Name = "cmbJenisKelamin";
-            this.cmbJenisKelamin.Size = new System.Drawing.Size(82, 21);
+            this.cmbJenisKelamin.Size = new System.Drawing.Size(150, 21);
             this.cmbJenisKelamin.TabIndex = 8;
             // 
             // cmbGolongan
             // 
             this.cmbGolongan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbGolongan.FormattingEnabled = true;
-            this.cmbGolongan.Location = new System.Drawing.Point(124, 253);
+            this.cmbGolongan.Location = new System.Drawing.Point(124, 256);
             this.cmbGolongan.Name = "cmbGolongan";
-            this.cmbGolongan.Size = new System.Drawing.Size(82, 21);
+            this.cmbGolongan.Size = new System.Drawing.Size(150, 21);
             this.cmbGolongan.TabIndex = 10;
             // 
             // txtNoKTP
@@ -144,7 +144,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(7, 204);
+            this.label10.Location = new System.Drawing.Point(6, 206);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(71, 13);
             this.label10.TabIndex = 20;
@@ -153,7 +153,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(7, 256);
+            this.label9.Location = new System.Drawing.Point(6, 259);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(53, 13);
             this.label9.TabIndex = 18;
@@ -169,7 +169,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(7, 285);
+            this.label8.Location = new System.Drawing.Point(6, 286);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(103, 13);
             this.label8.TabIndex = 15;
@@ -178,7 +178,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(7, 230);
+            this.label7.Location = new System.Drawing.Point(6, 233);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 13);
             this.label7.TabIndex = 14;
@@ -187,7 +187,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 178);
+            this.label6.Location = new System.Drawing.Point(6, 179);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(55, 13);
             this.label6.TabIndex = 13;
@@ -196,7 +196,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 152);
+            this.label5.Location = new System.Drawing.Point(6, 152);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(60, 13);
             this.label5.TabIndex = 12;
@@ -205,7 +205,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 129);
+            this.label4.Location = new System.Drawing.Point(6, 125);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 11;
@@ -214,7 +214,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 100);
+            this.label3.Location = new System.Drawing.Point(6, 100);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 10;
@@ -223,7 +223,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 74);
+            this.label2.Location = new System.Drawing.Point(6, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 9;
@@ -232,7 +232,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 48);
+            this.label1.Location = new System.Drawing.Point(6, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -240,42 +240,42 @@
             // 
             // dtpTanggalPembuatan
             // 
-            this.dtpTanggalPembuatan.Location = new System.Drawing.Point(124, 279);
+            this.dtpTanggalPembuatan.Location = new System.Drawing.Point(124, 283);
             this.dtpTanggalPembuatan.Name = "dtpTanggalPembuatan";
             this.dtpTanggalPembuatan.Size = new System.Drawing.Size(150, 20);
             this.dtpTanggalPembuatan.TabIndex = 11;
             // 
             // txtNoSIM
             // 
-            this.txtNoSIM.Location = new System.Drawing.Point(124, 227);
+            this.txtNoSIM.Location = new System.Drawing.Point(124, 230);
             this.txtNoSIM.Name = "txtNoSIM";
-            this.txtNoSIM.Size = new System.Drawing.Size(126, 20);
+            this.txtNoSIM.Size = new System.Drawing.Size(150, 20);
             this.txtNoSIM.TabIndex = 9;
             // 
             // txtTempatLahir
             // 
             this.txtTempatLahir.Location = new System.Drawing.Point(124, 97);
             this.txtTempatLahir.Name = "txtTempatLahir";
-            this.txtTempatLahir.Size = new System.Drawing.Size(126, 20);
+            this.txtTempatLahir.Size = new System.Drawing.Size(150, 20);
             this.txtTempatLahir.TabIndex = 4;
             // 
             // txtAlamat
             // 
             this.txtAlamat.Location = new System.Drawing.Point(124, 71);
             this.txtAlamat.Name = "txtAlamat";
-            this.txtAlamat.Size = new System.Drawing.Size(214, 20);
+            this.txtAlamat.Size = new System.Drawing.Size(268, 20);
             this.txtAlamat.TabIndex = 3;
             // 
             // txtNama
             // 
             this.txtNama.Location = new System.Drawing.Point(124, 45);
             this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(160, 20);
+            this.txtNama.Size = new System.Drawing.Size(268, 20);
             this.txtNama.TabIndex = 2;
             // 
             // btnSimpan
             // 
-            this.btnSimpan.Location = new System.Drawing.Point(12, 321);
+            this.btnSimpan.Location = new System.Drawing.Point(12, 327);
             this.btnSimpan.Name = "btnSimpan";
             this.btnSimpan.Size = new System.Drawing.Size(75, 23);
             this.btnSimpan.TabIndex = 12;
@@ -285,7 +285,7 @@
             // 
             // btnTampilkan
             // 
-            this.btnTampilkan.Location = new System.Drawing.Point(223, 321);
+            this.btnTampilkan.Location = new System.Drawing.Point(302, 327);
             this.btnTampilkan.Name = "btnTampilkan";
             this.btnTampilkan.Size = new System.Drawing.Size(108, 23);
             this.btnTampilkan.TabIndex = 13;
@@ -295,7 +295,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(111, 321);
+            this.button1.Location = new System.Drawing.Point(190, 327);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 23);
             this.button1.TabIndex = 14;
@@ -307,11 +307,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 356);
+            this.ClientSize = new System.Drawing.Size(422, 362);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnTampilkan);
             this.Controls.Add(this.btnSimpan);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmInputDataSIM";
             this.Text = "Input Data SIM";
             this.Load += new System.EventHandler(this.frmInputDataSIM_Load);
